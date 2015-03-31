5e24e153183d3ece74ec103e5ad52ace1775ddc3
wushuyi <wushuyi541@gmail.com>
2015-04-01 06:29:41 +0800

    add

---
+++ src/util/vendorPrefix.js
@@ -0,0 +1,26 @@
+/**
+ * Created by shuyi.wu on 2015/4/1.
+ */
+import engine from './detectEngine';
+var allPrefixJs,
+    allPrefixCss,
+    vendorPrefixJs,
+    vendorPrefixCss;
+
+allPrefixJs= {
+    trident: 'ms',
+    gecko: 'Moz',
+    webkit: 'Webkit',
+    presto: 'O'
+};
+allPrefixCss = {
+    trident: '-ms-',
+    gecko: '-moz-',
+    webkit: '-webkit-',
+    presto: '-o-'
+};
+vendorPrefixJs = allPrefixJs[engine];
+vendorPrefixCss = allPrefixCss[engine];
+
+export var js =  vendorPrefixJs;
+export var css =  vendorPrefixCss;
